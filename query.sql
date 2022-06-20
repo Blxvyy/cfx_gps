183d8f9977090fcc5737dd1746dc389d5761bd19
FACTS <78086344+HoaxFacts@users.noreply.github.com>
2022-06-20 12:29:00 -0700

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,1 +1,5 @@
-INSERT INTO `database`.`items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('gps', 'gps', '1', '0', '1');
+-- Put here the name of the database you're currently using. Or just remove it if it throws an error.
+USE "database_name";
+
+-- Is this throws an error change the weight to limit. The problem is probably fixed.
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('gps', 'gps', '1', '0', '1');
